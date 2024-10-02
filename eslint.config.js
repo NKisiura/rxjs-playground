5445c5702dcd5136af6ac90dabea7c3de7144048
Nikita Kisiura <nikitar156@gmail.com>
2024-10-01 22:44:37 -0400

    es-lint config in-progress

---
+++ eslint.config.js
@@ -0,0 +1,91 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      // TYPESCRIPT RULES
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+          custom: {
+            regex: "^(?!.*Interface$)",
+            match: true,
+          },
+          leadingUnderscore: "forbid",
+          trailingUnderscore: "forbid",
+        },
+        {
+          selector: "enum",
+          format: ["PascalCase"],
+          custom: {
+            regex: "^(?!.*Enum$)",
+            match: true,
+          },
+          leadingUnderscore: "forbid",
+          trailingUnderscore: "forbid",
+        },
+      ],
+
+      // ANGULAR RULES
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      "@angular-eslint/template/conditional-complexity": [
+        "error",
+        {
+          maxComplexity: 3,
+        },
+      ],
+      "@angular-eslint/template/cyclomatic-complexity": [
+        "error",
+        {
+          maxComplexity: 5,
+        },
+      ],
+      "@angular-eslint/template/attributes-order": ["error"],
+      "@angular-eslint/template/button-has-type": ["error"],
+      "@angular-eslint/template/no-duplicate-attributes": ["error"],
+      "@angular-eslint/template/no-inline-styles": ["error"],
+      "@angular-eslint/template/no-interpolation-in-attributes": ["error"],
+      "@angular-eslint/template/prefer-control-flow": ["error"],
+      "@angular-eslint/template/prefer-self-closing-tags": ["error"],
+      "@angular-eslint/template/use-track-by-function": ["error"],
+    },
+  }
+);
