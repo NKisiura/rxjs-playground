817c6969ff5c7daf7c8c6ecb9d3ef763b13e099c
Nikita Kisiura <nikitar156@gmail.com>
2024-10-11 14:06:09 -0400

    without baseHref

---
+++ chunk-RD2DQ7OB.js
@@ -0,0 +1,1 @@
+import{$a as i,Ra as a,Sa as o,_ as n,bb as s}from"./chunk-7G64M734.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-alphabet-invasion"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(a(0,"p"),i(1,"alphabet-invasion works!"),o())},changeDetection:0})};export{p as AlphabetInvasionComponent};
