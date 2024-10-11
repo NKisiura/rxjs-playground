cb589e18134ae0506fd4587bf513c7e783b4ed46
Nikita Kisiura <nikitar156@gmail.com>
2024-10-11 13:24:59 -0400

    Auto-generated commit

---
+++ chunk-PFIOGSJX.js
@@ -0,0 +1,1 @@
+import{Qa as a,Ra as o,Xa as i,Z as n,Za as s}from"./chunk-RJJLLLRV.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-alphabet-invasion"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(a(0,"p"),i(1,"alphabet-invasion works!"),o())},changeDetection:0})};export{p as AlphabetInvasionComponent};
